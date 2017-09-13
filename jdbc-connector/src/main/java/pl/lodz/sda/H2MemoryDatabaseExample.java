b98f7f80563015818f862fc11e7c0a01938add78
marcinerbel <marcinerbel90@gmail.com>
2017-09-13 15:41:33 +0200

    init commit

---
+++ jdbc-connector/src/main/java/pl/lodz/sda/H2MemoryDatabaseExample.java
@@ -0,0 +1,93 @@
+package pl.lodz.sda;
+
+import java.sql.*;
+
+public class H2MemoryDatabaseExample {
+
+    private static final String DB_CONNECTION = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+    private static final String DB_USER = "sa";
+    private static final String DB_PASSWORD = "";
+
+    public static void main(String[] args) throws Exception {
+        try {
+            insertWithStatement();
+            insertWithPreparedStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertWithPreparedStatement() throws SQLException {
+        Connection connection = getDBConnection();
+
+        String CreateQuery = "CREATE TABLE PERSON(id int primary key, name varchar(255))";
+        String InsertQuery = "INSERT INTO PERSON" + "(id, name) values" + "(?,?)";
+        String SelectQuery = "select * from PERSON";
+
+        try {
+            connection.setAutoCommit(false);
+            PreparedStatement createPreparedStatement = connection.prepareStatement(CreateQuery);
+            createPreparedStatement.executeUpdate();
+            createPreparedStatement.close();
+
+            PreparedStatement insertPreparedStatement = connection.prepareStatement(InsertQuery);
+            insertPreparedStatement.setInt(1, 1);
+            insertPreparedStatement.setString(2, "Jose");
+            insertPreparedStatement.executeUpdate();
+            insertPreparedStatement.close();
+
+            PreparedStatement selectPreparedStatement = connection.prepareStatement(SelectQuery);
+            ResultSet rs = selectPreparedStatement.executeQuery();
+            System.out.println("H2 In-Memory Database inserted through PreparedStatement");
+            while (rs.next()) {
+                System.out.println("Id " + rs.getInt("id") + " Name " + rs.getString("name"));
+            }
+            selectPreparedStatement.close();
+            connection.commit();
+
+        } catch (SQLException e) {
+            System.out.println("Exception Message " + e.getLocalizedMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
+    }
+
+    private static void insertWithStatement() throws SQLException {
+        Connection connection = getDBConnection();
+        try {
+            connection.setAutoCommit(false);
+            Statement stmt = connection.createStatement();
+            stmt.execute("CREATE TABLE PERSON(id int primary key, name varchar(255))");
+            stmt.execute("INSERT INTO PERSON(id, name) VALUES(1, 'Anju')");
+            stmt.execute("INSERT INTO PERSON(id, name) VALUES(2, 'Sonia')");
+            stmt.execute("INSERT INTO PERSON(id, name) VALUES(3, 'Asha')");
+
+            ResultSet rs = stmt.executeQuery("select * from PERSON");
+            System.out.println("H2 In-Memory Database inserted through Statement");
+            while (rs.next()) {
+                System.out.println("Id " + rs.getInt("id") + " Name " + rs.getString("name"));
+            }
+
+            stmt.execute("DROP TABLE PERSON");
+            stmt.close();
+            connection.commit();
+        } catch (SQLException e) {
+            System.out.println("Exception Message " + e.getLocalizedMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
+    }
+
+    private static Connection getDBConnection() {
+        try {
+            return DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
